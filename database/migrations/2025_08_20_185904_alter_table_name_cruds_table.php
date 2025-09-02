3c204bb3410f160f42a4e0936ada77c3e8415032
Halil İbrahim BELKIR <ibrahim@halilbelkir.com>
2025-09-02 13:50:40 +0300

    refactor

--- database/migrations/2025_08_20_185904_alter_table_name_cruds_table.php
+++ database/migrations/2025_08_20_185904_alter_table_name_cruds_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -14,6 +15,41 @@ return new class extends Migration
         Schema::table('cruds', function (Blueprint $table) {
             $table->string('table_name')->nullable()->after('slug');;
         });
+
+        DB::table('cruds')->insert(
+            [
+                'title'      => 'Kullanıcılar',
+                'slug'       => 'users',
+                'table_name' => 'users',
+                'status'     => 1,
+                'main'       => 1,
+                'verified' => true
+            ],
+            [
+                'title'      => 'Yetkiler',
+                'slug'       => 'role-groups',
+                'table_name' => 'role-groups',
+                'status'     => 1,
+                'main'       => 1,
+                'verified' => true
+            ],
+            [
+                'title'      => 'Menü',
+                'slug'       => 'menus',
+                'table_name' => 'menus',
+                'status'     => 1,
+                'main'       => 1,
+                'verified' => true
+            ],
+            [
+                'title'      => 'Modüller',
+                'slug'       => 'cruds',
+                'table_name' => 'cruds',
+                'status'     => 1,
+                'main'       => 1,
+                'verified' => true
+            ]
+        );
     }
 
     /**
