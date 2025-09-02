fe7b3e4761b5092458400941a886df47d9cb4254
Halil İbrahim BELKIR <ibrahim@halilbelkir.com>
2025-09-02 13:27:32 +0300

    refactor

--- database/migrations/2025_08_16_210134_edit_users_table_role_group_id.php
+++ database/migrations/2025_08_16_210134_edit_users_table_role_group_id.php
@@ -13,7 +13,8 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table)
         {
-            $table->foreign('role_group_id','foreign_role_groups')->references('id')->on('role_groups')->after('password');;
+            $table->unsignedBigInteger('role_group_id')->after('password');
+            $table->foreign('role_group_id')->references('id')->on('role_groups');
         });
     }
 
