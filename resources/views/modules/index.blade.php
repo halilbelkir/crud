ecc5a8ebc7413440f0b0e18ab840ac0c1aa82db8
Halil İbrahim BELKIR <ibrahim@halilbelkir.com>
2025-09-07 17:12:23 +0300

    refactor

--- resources/views/modules/index.blade.php
+++ resources/views/modules/index.blade.php
@@ -1,12 +1,12 @@
 @extends('crudPackage::layout.main',['activePage' => $crud->display_plural])
 @section('content')
+    @if($crud->content) {!! $crud->content !!} @endif
     <div class="card">
         <div class="card-header border-0 pt-6">
             <div class="card-title">
                 <div class="d-flex align-items-center position-relative my-1">
                     <i class="ki-outline ki-magnifier fs-3 position-absolute ms-5"></i>
-                    <input type="text" data-kt-data-table-filter="search"
-                           class="form-control form-control-solid w-250px ps-13" placeholder="Arama Yap">
+                    <input type="text" data-kt-data-table-filter="search" class="form-control form-control-solid w-250px ps-13" placeholder="Arama Yap">
                 </div>
             </div>
             <div class="card-toolbar">
