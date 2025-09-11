da687d61b1907450b64b57028a3a458eaf02ed19
Halil İbrahim BELKIR <ibrahim@halilbelkir.com>
2025-09-11 20:30:42 +0300

    Halil İbrahim BELKIR tarafindan yapildi

--- resources/views/menus/overview.blade.php
+++ resources/views/menus/overview.blade.php
@@ -3,10 +3,11 @@
         @foreach($value->parentItems as $parentItem)
             <li class="parent" data-id="{{$parentItem->id}}">
                 {{$parentItem->title}}
+                <small> - {{$parentItem->route}}</small>
                 <ul class="children">
                     @if($parentItem->children)
                         @foreach($parentItem->children as $children)
-                            <li data-id="{{$children->id}}">{{$children->title}}</li>
+                            <li data-id="{{$children->id}}">{{$children->title}} <small> - {{$children->route}}</small></li>
                         @endforeach
                     @endif
                 </ul>
