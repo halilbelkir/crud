15ff7eed33632c6b004c0f7315efb704730efe4a
Halil İbrahim BELKIR <ibrahim@halilbelkir.com>
2025-10-24 16:26:30 +0300

    repeater required zorunluluğu kaldırma

--- src/Http/Controllers/ModuleController.php
+++ src/Http/Controllers/ModuleController.php
@@ -82,7 +82,7 @@ class ModuleController extends Controller
 
                 foreach($details as $detail)
                 {
-                    if (strstr('required',$detail->validation))
+                    if (isset($detail->validation) && strstr('required',$detail->validation))
                     {
                         $detail->required = 1;
                     }
@@ -467,7 +467,7 @@ class ModuleController extends Controller
 
                     foreach($details as $detail)
                     {
-                        if (strstr('required',$detail->validation))
+                        if (isset($detail->validation) && strstr('required',$detail->validation))
                         {
                             $detail->required = 1;
                         }
@@ -480,7 +480,7 @@ class ModuleController extends Controller
                         $formTypeR  = FormType::find($detail->form_type_id);
                         $typeR      = $formTypeR->key;
                         $rElements .= '<div class="form-group '.($detail->form_type_id == 13 ? 'd-none' : null) .' '.(isset($detail->class) ? $detail->class : 'col').' mb-7 fv-plugins-icon-container">';
-                        $rElements .= '<label class=" '.($column->required == 1 ? 'required' : null) .' w-100 fw-semibold fs-6 mb-2" for="repeater_'.$detail->column_name.'">'.$detail->title.'</label>';
+                        $rElements .= '<label class=" '.($detail->required == 1 ? 'required' : null) .' w-100 fw-semibold fs-6 mb-2" for="repeater_'.$detail->column_name.'">'.$detail->title.'</label>';
                         $rElements .= view('crudPackage::formTypes.'. $formTypeR->group,['column' => $detail,'formType' => $formTypeR,'type' => $typeR],compact('crud'))->render();
                         $rElements .= '</div>';
                     }
